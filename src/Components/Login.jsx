c5609bc6c0c7503a0dfabeb1a7d812825557511e
KylinLee <lee.kylin@outlook.com>
2020-09-24 18:21:29 +0800

    add comment

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,7 +5,11 @@ import "./login.css"
 class Login extends React.Component {
     constructor(props) {
         super(props);
+
+        // 将redux-store中的状态映射到组件中
         this.state = store.getState();
+
+        // 当redux-store发生变化，更新组件的store
         store.subscribe(() => {
             this.setState({
                 ...store.getState()
@@ -15,12 +19,22 @@ class Login extends React.Component {
 
     switchLogStatus() {
         const {isLogin} = store.getState()
-            store.dispatch((() => {
-                    return {
-                        type: isLogin ? "LOGOUT" : "LOGIN"
-                    }
-                })()
-            )
+        // dispatch一个对象（action对象），这个对象包含type属性
+
+        // 下面这个函数相当于自执行的Action Creator（创建action对象）
+        // 可以将其放入reducer中，再引入，Vuex建议这样做
+        /*(() => {
+            return {
+                type: isLogin ? "LOGOUT" : "LOGIN"
+            }
+        })()*/
+
+        store.dispatch((() => {
+                return {
+                    type: isLogin ? "LOGOUT" : "LOGIN"
+                }
+            })()
+        )
     }
 
     render() {
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import {createStore} from "@reduxjs/toolkit"
 import reducer from "./reducer"
 
+// 使用reducer初始化redux-store
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 export default store
